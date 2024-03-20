ea1ae47eddac72f8493b3aeb87e1b27643729cfa
GULFAIZ <143407759+GULFAIZ@users.noreply.github.com>
2024-03-20 23:44:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ const adminAuthentication = (req,res,next)=>{
   }
 };
 
+const userAuthentication = (req, res, next) => {
+  const { username, password } = req.headers;
+  const user = USERS.find(u => u.username === username && u.password === password);
+  if (user) {
+    req.user = user;  // Add user object to the request
+    next();
+  } else {
+    res.status(403).json({ message: 'User authentication failed' });
+  }
+};
+
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
 });
